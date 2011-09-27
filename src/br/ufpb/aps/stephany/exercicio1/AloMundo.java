6c06d17c9a7e61a073f9d4cbd786d023c3039153
Stephany Vitorio <stephany.vitorio@dce.ufpb.br>
2011-09-27 15:07:58 -0300

    AloMundo teste

---
+++ src/br/ufpb/aps/stephany/exercicio1/AloMundo.java
@@ -0,0 +1,13 @@
+package br.ufpb.aps.stephany.exercicio1;
+
+public class AloMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
